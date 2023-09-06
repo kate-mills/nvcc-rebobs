3199ae8de2ed1deb53495949d1a9f35a20411be9
Kate Mills <helloworldloveksm@icloud.com>
2023-09-06 16:13:19 -0700

    updated SEO image

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import IndexView from 'views/IndexView'
 
 import Seo from 'components/Seo'
-//import SeoImg from 'images/website.jpg'
+import SeoImg from 'images/2023/Team-Photo-Skyline.webp'
 
 const IndexPage = () => {
   return <IndexView />
@@ -16,6 +16,7 @@ export const Head = ({location}) => {
       location={location}
       pageContext={{
       title: 'Home',
+        image: {SeoImg},
         description: `Napa Valley Composite Cycling Team is a NICA affiliated, high-school age MTB team. Whether you are a beginner, or have been racing before, our team would love to have you join us as we compete against highschools from around California. Students from ALL Napa High Schools are welcome to join!`,
       }}
     />

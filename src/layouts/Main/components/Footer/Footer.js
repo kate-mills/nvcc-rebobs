a0c88d8ef05ea77c859f5549b732f54cc7b0fddd
Kate Mills <helloworldloveksm@icloud.com>
2023-09-10 12:27:45 -0700

    Link to instagram

--- src/layouts/Main/components/Footer/Footer.js
+++ src/layouts/Main/components/Footer/Footer.js
@@ -1,66 +1,31 @@
 import React from 'react'
 import Grid from '@mui/material/Grid'
 import Box from '@mui/material/Box'
-import Button from '@mui/material/Button'
-import Link from '@mui/material/Link'
+import Fab from '@mui/material/Fab'
+import InstaIcon from '@mui/icons-material/Instagram'
 import Typography from '@mui/material/Typography'
-import GatsbyLink from 'components/Link'
-
-import logoImg from 'images/rebob.webp'
 
 const Footer = () => {
-
   return (
     <Grid container spacing={2}>
       <Grid item xs={12}>
         <Box
           display={'flex'}
-          justifyContent={'space-between'}
           alignItems={'center'}
           width={1}
           flexDirection={{ xs: 'column', sm: 'row' }}
         >
-          <Box
-            display={'flex'}
-            component={GatsbyLink}
-            to="/"
-            title="NAPA VALLEY COMPOSITE CYCLING"
-            width={80}
-          >
-            <Box
-              component={'img'}
-              src={logoImg}
-              height={1}
-              width={1}
-            />
-          </Box>
-          <Box display="flex" flexWrap={'wrap'} alignItems={'center'}>
-            <Box marginTop={1} marginRight={2}>
-              <Link underline="none" component={GatsbyLink} to="/" color="text.primary" variant={'subtitle2'} > Home</Link>
-            </Box>
-            <Box marginTop={1} marginRight={2}>
-              <Link
-                underline="none"
-                component={GatsbyLink}
-                to={'/about'}
-                color="text.primary"
-                variant={'subtitle2'}
-              >
-                About
-              </Link>
-            </Box>
-            <Box marginTop={1}>
-              <Button
-                variant="outlined"
-                color="primary"
-                component={GatsbyLink}
-                target="blank"
-                to={'/contact'}
-                size="small"
-              >
-                Contact
-              </Button>
-            </Box>
+          <Box marginTop={1}>
+            <Fab
+              color="secondary"
+              sx={{ boxShadow: 'none' }}
+              component={'a'}
+              href="https://www.instagram.com/napavalleycomposite/"
+              target="blank"
+              title="Go to Instagram.com"
+            >
+              <InstaIcon />
+            </Fab>
           </Box>
         </Box>
       </Grid>

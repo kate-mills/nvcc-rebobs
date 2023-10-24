f8eaeea113558526419e0efd7de4f92bd3b135b6
Kate Mills <helloworldloveksm@icloud.com>
2023-10-23 18:35:14 -0700

    Structured data

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -6,15 +6,26 @@ const Seo = ({ location, params, data, pageContext = {} }) => {
 
   const defaultTitle = `Napa Valley Composite Cycling Team | Napa, CA`
 
-  const seo = { 
-    title: ((!title) ? defaultTitle : `${title} | ${defaultTitle}`),
+  const seo = {
+    title: !title ? defaultTitle : `${title} | ${defaultTitle}`,
     img: image || `${canonicalUrl}/team-photo.webp`,
     canonical: `${canonicalUrl}${location.pathname}`,
     description:
       description ||
-      'Napa Valley Composite Cycling Team is a NICA affiliated, high-school age MTB team. Whether you are a beginner, or have been racing before, our team would love to have you join us as we compete against highschools from around California. Students from ALL Napa High Schools are welcome to join!',
+      'Napa Valley Composite Cycling Team is a NICA affiliated, high-school age XC MTB team. Whether you are a beginner, or have been racing before, our team would love to have you join us as we compete against highschools from around California. Students from ALL Napa High Schools are welcome to join!',
   }
 
+  const baseSchema = [
+    {
+      "@context" : "https://schema.org",
+        "@type" : "SportsTeam",
+         name: "Napa Valley Composite Cycling Team",
+         url: 'https://www.napavalleycompositecycling.com/',
+         alternateName: 'The Rebobs',
+         sport: 'XC MTB',
+         memberOf: [ { '@type': 'SportsOrganization', name: 'NICA' } ],
+    }
+  ]
   return (
     <>
       <meta name="description" content={seo.description} />
@@ -30,7 +41,7 @@ const Seo = ({ location, params, data, pageContext = {} }) => {
       <meta name="og:site_name" content="Napa Valley Composite Cycling Team" />
 
       <meta name="fb_admins_meta_tag" content="napavalleycompositecycling" />
-      <meta name="keywords" content="Cycling, MTB, NICA" />
+      <meta name="keywords" content="Cycling, XC MTB, NICA" />
       <meta property="fb:admins" content="napavalleycompositecycling" />
 
       <meta name="twitter:card" content="summary_large_image" />
@@ -38,6 +49,10 @@ const Seo = ({ location, params, data, pageContext = {} }) => {
       <meta name="twitter:title" content={seo.title} />
       <meta name="twitter:description" content={seo.description} />
       <link rel="canonical" href={seo.canonical}/>
+
+      <script type="application/ld+json">
+        {JSON.stringify(baseSchema) }
+      </script>
     </>
   )
 }

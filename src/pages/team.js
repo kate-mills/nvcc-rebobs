3a8756d0df5307248d540b0043e12cb930ccd7cd
Kate Mills <helloworldloveksm@icloud.com>
2023-11-17 13:59:20 -0800

    single word title

--- src/pages/team.js
+++ src/pages/team.js
@@ -14,7 +14,7 @@ export const Head = ({location}) => {
     <Seo
       location={location}
       pageContext={{
-      title: 'Team Apps',
+      title: 'Team',
         description: `Easily access your Rebob team accounts from here. Connect with teammates, stay up-to-date on team news and announcements, and register for upcoming races and events.`,
       }}
     />

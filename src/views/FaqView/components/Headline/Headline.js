3f64b1b39bcebe64200caa8acc2c9a956cf9310a
Kate Mills <helloworldloveksm@icloud.com>
2023-11-18 17:13:02 -0800

    Heading elements in sequential-descending order

--- src/views/FaqView/components/Headline/Headline.js
+++ src/views/FaqView/components/Headline/Headline.js
@@ -5,10 +5,10 @@ import Typography from '@mui/material/Typography';
 const Headline = () => {
   return (
     <Box>
-      <Typography variant="h2" align={'center'} fontWeight={700} gutterBottom>
+      <Typography component="p" variant="h2" align={'center'} fontWeight={700} gutterBottom>
         Have a question?
       </Typography>
-      <Typography variant="h6" align={'center'} color={'textSecondary'}>
+      <Typography component="h1" variant="h6" align={'center'} color={'textSecondary'}>
 Discover answers to commonly asked questions regarding our team, practices, coaches, and other pertinent details.
       </Typography>
     </Box>
